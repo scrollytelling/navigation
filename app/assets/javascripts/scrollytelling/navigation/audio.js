fe3f2cfcdb5efed74cf9a73941b9ab0adabc494c
Joost Baaij <joost@spacebabies.nl>
2017-08-14 09:41:00 +0100

    show the actual audio state
    and don't start assuming it's in the playing state.

--- app/assets/javascripts/scrollytelling/navigation/audio.js
+++ app/assets/javascripts/scrollytelling/navigation/audio.js
@@ -1,18 +1,27 @@
 $.widget('scrollytelling.audioControl', {
+  showState: function() {
+    var icon = $('#scrollytelling-audio', this.element);
+
+    if(pageflow.settings.get('volume') > 0) {
+      icon.removeClass('audio-playing').addClass('audio-muted');
+    } else {
+      icon.removeClass('audio-muted').addClass('audio-playing');
+    }
+  },
+
   _create: function() {
-    $('#scrollytelling-audio', this.element).on('click', this.toggleAudio);
+    this.showState();
+    $('#scrollytelling-audio', this.element).on('click', this.toggleAudio.bind(this));
   },
 
   toggleAudio: function(event) {
-    event.preventDefault();
-    var target = $(event.target);
-
     if(pageflow.settings.get('volume') > 0) {
       pageflow.settings.set('volume', 0);
-      target.removeClass('audio-playing').addClass('audio-muted');
     } else {
       pageflow.settings.set('volume', 1);
-      target.removeClass('audio-muted').addClass('audio-playing');
     }
+
+    event.preventDefault();
+    this.showState();
   }
 });
