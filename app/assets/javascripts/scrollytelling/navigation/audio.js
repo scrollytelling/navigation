e04c598ff626f1e00af0c23d694a93af25736947
Joost Baaij <joost@spacebabies.nl>
2017-12-29 15:48:23 +0100

    WIP rabota play fixes

--- app/assets/javascripts/scrollytelling/navigation/audio.js
+++ app/assets/javascripts/scrollytelling/navigation/audio.js
@@ -10,18 +10,61 @@ $.widget('scrollytelling.audioControl', {
   },
 
   _create: function() {
-    this.showState();
+    var showState = this.showState;
+    showState();
     $('#scrollytelling-audio', this.element).on('click', this.toggleAudio.bind(this));
+
+    pageflow.events.on('media:userGestureRequired', function(player) {
+      console.log("userGestureRequired");
+
+      var icon = $('#scrollytelling-audio', this.element);
+
+      icon.addClass('audio-needs-user-gesture');
+
+      var clickHandler = $('#scrollytelling-audio', this.element).on('click', function (playerObject, event) {
+         $( this ).unbind( event, clickHandler );
+
+         if(icon.hasClass('audio-needs-user-gesture')) {
+           icon.removeClass('audio-needs-user-gesture');
+           playerObject.originalPlay.apply(playerObject.player, playerObject.arguments);
+           pageflow.settings.set('volume', 1);
+           showState();
+         }
+       }.bind(this, player));
+    });
+
+    // pageflow.events.on('page:change', function() {
+    //    console.log("page change");
+    //    var icon = $('#scrollytelling-audio', this.element);
+    //    icon.removeClass('audio-needs-user-gesture');
+    // });
   },
 
+  // userGestureRequired(player) {
+  //   icon.addClass('audio-needs-user-gesture');
+  //
+  //    $('#scrollytelling-audio', this.element).on('click', function (player) {
+  //      icon.removeClass('audio-needs-user-gesture');
+  //      player.play();
+  //    }.bind(this, player));
+  // }
+
   toggleAudio: function(event) {
-    if(pageflow.settings.get('volume') > 0) {
-      pageflow.settings.set('volume', 0);
-    } else {
-      pageflow.settings.set('volume', 1);
+    event.preventDefault();
+
+    var icon = $('#scrollytelling-audio', this.element);
+
+    if(!icon.hasClass('audio-needs-user-gesture')) {
+
+      if(pageflow.settings.get('volume') > 0) {
+        pageflow.settings.set('volume', 0);
+      } else {
+        pageflow.settings.set('volume', 1);
+      }
+
+      this.showState();
+
     }
 
-    event.preventDefault();
-    this.showState();
   }
 });
